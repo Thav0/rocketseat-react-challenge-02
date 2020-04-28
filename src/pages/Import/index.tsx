4dd985ec9d590b42782f01be0e9cadaabaef787b
Thav0 <gustavodnz035@gmail.com>
2020-04-28 13:39:36 -0300

    Challenge completed

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,35 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
+
+    if (!uploadedFiles.length) {
+      return;
+    }
+
+    const { file, name } = uploadedFiles[0];
+    await data.append('file', file);
 
     // TODO
-
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
     // TODO
+    files.forEach(file => {
+      setUploadedFiles([
+        ...uploadedFiles,
+        {
+          file,
+          name: file.name,
+          readableSize: `${file.size} bytes`,
+        },
+      ]);
+    });
   }
 
   return (
